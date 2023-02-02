e49e3c14c331340e6276c63e2b54e6fac9080d93
Eyal-donut <LilaEyal@gmail.com>
2023-02-02 21:03:08 +0100

    clouds_done

--- minecraft.js
+++ minecraft.js
@@ -43,10 +43,20 @@ Material.createMaterial('rock', 8, 8, 17, 21)
 Material.createMaterial('rock', 9, 9, 22, 22)
 Material.createMaterial('rock', 10, 10, 23, 23)
 
-Material.createMaterial('wood',)
+Material.createMaterial('wood', 7 ,10, 30, 30)
+Material.createMaterial('leaf', 3 , 6, 29, 31)
 
 
 
-Material.createMaterial('clouds',)
-Material.createMaterial('sun', )
+Material.createMaterial('cloud', 3, 4, 10, 12)
+Material.createMaterial('cloud', 2, 3, 12, 14)
+Material.createMaterial('cloud', 1, 2, 13, 16)
+
+Material.createMaterial('cloud', 1, 2, 24, 25)
+Material.createMaterial('cloud', 2, 3, 22, 25)
+Material.createMaterial('cloud', 3, 4, 24, 27)
+Material.createMaterial('cloud', 2, 3, 26, 26)
+
+Material.createMaterial('sun', 1, 4, 2, 5) 
+
 
