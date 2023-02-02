ab8057165dcd3440faba18562032662985237e5b
Eyal-donut <LilaEyal@gmail.com>
2023-02-02 20:38:34 +0100

    created_water

--- minecraft.js
+++ minecraft.js
@@ -9,7 +9,7 @@ class Material{
         this.active = active;
     }
 
-    static createMaterial(gameGrid, type, rowStart, rowEnd, columnStart, columnEnd){
+    static createMaterial(type, rowStart, rowEnd, columnStart, columnEnd){
 
         for (let row = rowStart; row <= rowEnd; ++row){
             for(let column = columnStart; column <= columnEnd; ++column) {
@@ -23,4 +23,30 @@ class Material{
     }
 }
 
-Material.createMaterial(gameGrid, 'grass', 1, 2, 1, 5)
+Material.createMaterial('dirt', 12, 15, 1, 14)
+Material.createMaterial('dirt', 12, 15, 23, 37)
+Material.createMaterial('dirt', 13, 15, 13, 15)
+Material.createMaterial('dirt', 13, 15, 22, 23)
+
+
+
+Material.createMaterial('grass', 11, 11, 1, 14)
+Material.createMaterial('grass', 11, 11, 23, 37)
+
+Material.createMaterial('water', 11, 12, 15, 22)
+Material.createMaterial('water', 13, 15, 16, 21)
+
+Material.createMaterial('rock', 10, 10, 14, 14)
+Material.createMaterial('rock', 9, 9, 15, 15)
+Material.createMaterial('rock', 8, 8, 16, 16)
+Material.createMaterial('rock', 8, 8, 17, 21)
+Material.createMaterial('rock', 9, 9, 22, 22)
+Material.createMaterial('rock', 10, 10, 23, 23)
+
+Material.createMaterial('wood',)
+
+
+
+Material.createMaterial('clouds',)
+Material.createMaterial('sun', )
+
