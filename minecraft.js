ee1fba67cdd14cac8ea618f9615a581aa8196d1e
Eyal-donut <LilaEyal@gmail.com>
2023-02-03 22:11:12 +0100

    17

--- minecraft.js
+++ minecraft.js
@@ -1,9 +1,12 @@
 //--------------------------------------------------------global variable -------------------------------------------------------------
 let activeToolOrMaterial = "dirt";
 
+//----------------------------------------------------------Restart-Button---------------------------------------------------------------
+const restart = document.getElementById("restart");
+
 //--------------------------------------------------------Grid variables------------------------------------------------------------------
 
-const gameGrid = document.getElementById("game-grid");
+let gameGrid = document.getElementById("game-grid");
 const totalGridRows = 15;
 const totalGridColumns = 35;
 
@@ -21,7 +24,6 @@ const dirtInventory = document.getElementById("dirt-inventory");
 const grassInventory = document.getElementById("grass-inventory");
 
 //-----------------------------------------------------------------------------------------------------------------------------------------
-
 
 //!---------------------------------------------------------------Global functions -------------------------------------------------------
 
@@ -48,6 +50,9 @@ function addToInventory(type) {
 }
 
 function removeMaterial(element, type) {
+  if (element.className === "empty") {
+    element.classList.remove("empty");
+  }
   if (
     (activeToolOrMaterial === axe && element.className === "wood") ||
     (activeToolOrMaterial === axe && element.className === "leaf") ||
@@ -62,37 +67,35 @@ function removeMaterial(element, type) {
 }
 
 function addMaterial(clickTarget) {
+  if (activeToolOrMaterial === waterInventory && waterInventory.innerText > 0) {
+    clickTarget.classList.add("water");
+    --waterInventory.innerText;
+  }
+  if (activeToolOrMaterial === woodInventory && woodInventory.innerText > 0) {
+    clickTarget.classList.add("wood");
+    --woodInventory.innerText;
+  }
+  if (activeToolOrMaterial === rockInventory && rockInventory.innerText > 0) {
+    clickTarget.classList.add("rock");
+    --rockInventory.innerText;
+  }
+  if (activeToolOrMaterial === leafInventory && leafInventory.innerText > 0) {
+    clickTarget.classList.add("leaf");
+    --leafInventory.innerText;
+  }
+  if (activeToolOrMaterial === dirtInventory && dirtInventory.innerText > 0) {
+    clickTarget.classList.add("dirt");
+    --dirtInventory.innerText;
+  }
+  if (activeToolOrMaterial === grassInventory && grassInventory.innerText > 0) {
+    clickTarget.classList.add("grass");
+    --grassInventory.innerText;
+  }
 
-    if (activeToolOrMaterial === waterInventory && waterInventory.innerText > 0) {
-        clickTarget.classList.add('water');
-        --waterInventory.innerText
-    }
-    if (activeToolOrMaterial === woodInventory && woodInventory.innerText > 0) {
-        clickTarget.classList.add('wood');
-        --woodInventory.innerText
-    }
-    if (activeToolOrMaterial === rockInventory && rockInventory.innerText > 0) {
-        clickTarget.classList.add('rock');
-        --rockInventory.innerText
-    }
-    if (activeToolOrMaterial === leafInventory && leafInventory.innerText > 0) {
-        clickTarget.classList.add('leaf');
-        --leafInventory.innerText
-    }
-    if (activeToolOrMaterial === dirtInventory && dirtInventory.innerText > 0) {
-        clickTarget.classList.add('dirt');
-        --dirtInventory.innerText
-    }
-    if (activeToolOrMaterial === grassInventory && grassInventory.innerText > 0) {
-        clickTarget.classList.add('grass');
-        --grassInventory.innerText
-    }
-
-    
-    console.log(clickTarget)
+  console.log(clickTarget);
 }
 
-//!---------------------------------------------------Window Event listener- "Click to activate"-----------------------------------------------
+//!--------------------------------------------------------------- Event listeners--------------------------------------------------------------
 
 document.addEventListener("click", function (e) {
   if (e.target === axe) activeToolOrMaterial = axe;
@@ -105,6 +108,10 @@ document.addEventListener("click", function (e) {
   if (e.target === dirtInventory) activeToolOrMaterial = dirtInventory;
   if (e.target === grassInventory) activeToolOrMaterial = grassInventory;
   if (e.target === leafInventory) activeToolOrMaterial = leafInventory;
+});
+
+restart.addEventListener("click", function(){
+  window.location.reload();
 });
 
 //?--------------------------------------------------------------------Class Grid -------------------------------------------------------------
@@ -137,58 +144,55 @@ class Grid {
       }
     }
   }
-
   static gridClickHandler() {
     gameGrid.addEventListener("click", function (e) {
       removeMaterial(e.target, e.target.className);
-      if (e.target.className === "empty") {
-        e.target.classList.remove("empty");
-        addMaterial(e.target);
-      }
+      addMaterial(e.target);
     });
   }
-}
-Grid.createEmptyGrid();
-Grid.gridClickHandler();
 
+
+  static initiate() {
+    Grid.createEmptyGrid();
+    Grid.gridClickHandler();
+    Grid.createMap("dirt", 12, 15, 1, 14);
+    Grid.createMap("dirt", 12, 15, 23, 37);
+    Grid.createMap("dirt", 13, 15, 13, 15);
+    Grid.createMap("dirt", 13, 15, 22, 23);
+
+    Grid.createMap("grass", 11, 11, 1, 14);
+    Grid.createMap("grass", 11, 11, 23, 37);
+
+    Grid.createMap("water", 11, 12, 15, 22);
+    Grid.createMap("water", 13, 15, 16, 21);
+
+    Grid.createMap("rock", 10, 10, 14, 14);
+    Grid.createMap("rock", 9, 9, 15, 15);
+    Grid.createMap("rock", 8, 8, 16, 16);
+    Grid.createMap("rock", 8, 8, 17, 21);
+    Grid.createMap("rock", 9, 9, 22, 22);
+    Grid.createMap("rock", 10, 10, 23, 23);
+
+    Grid.createMap("wood", 7, 10, 30, 30);
+    Grid.createMap("leaf", 3, 6, 29, 31);
+
+    Grid.createMap("cloud", 3, 4, 10, 12);
+    Grid.createMap("cloud", 2, 3, 12, 14);
+    Grid.createMap("cloud", 1, 2, 13, 16);
+
+    Grid.createMap("cloud", 1, 2, 24, 25);
+    Grid.createMap("cloud", 2, 3, 22, 25);
+    Grid.createMap("cloud", 3, 4, 24, 27);
+    Grid.createMap("cloud", 2, 3, 26, 26);
+
+    Grid.createMap("sun", 1, 4, 2, 5);
+  }
+}
+Grid.initiate();
 
 //?---------------------------------------------------------------------------------------------------------------------------------------
-
 
 //click event on tool:
 //1. cursor becomes the tool.
 
-
 //---------------------------------------------------------------------Created map-----------------------------------------------------
-
-Grid.createMap("dirt", 12, 15, 1, 14);
-Grid.createMap("dirt", 12, 15, 23, 37);
-Grid.createMap("dirt", 13, 15, 13, 15);
-Grid.createMap("dirt", 13, 15, 22, 23);
-
-Grid.createMap("grass", 11, 11, 1, 14);
-Grid.createMap("grass", 11, 11, 23, 37);
-
-Grid.createMap("water", 11, 12, 15, 22);
-Grid.createMap("water", 13, 15, 16, 21);
-
-Grid.createMap("rock", 10, 10, 14, 14);
-Grid.createMap("rock", 9, 9, 15, 15);
-Grid.createMap("rock", 8, 8, 16, 16);
-Grid.createMap("rock", 8, 8, 17, 21);
-Grid.createMap("rock", 9, 9, 22, 22);
-Grid.createMap("rock", 10, 10, 23, 23);
-
-Grid.createMap("wood", 7, 10, 30, 30);
-Grid.createMap("leaf", 3, 6, 29, 31);
-
-Grid.createMap("cloud", 3, 4, 10, 12);
-Grid.createMap("cloud", 2, 3, 12, 14);
-Grid.createMap("cloud", 1, 2, 13, 16);
-
-Grid.createMap("cloud", 1, 2, 24, 25);
-Grid.createMap("cloud", 2, 3, 22, 25);
-Grid.createMap("cloud", 3, 4, 24, 27);
-Grid.createMap("cloud", 2, 3, 26, 26);
-
-Grid.createMap("sun", 1, 4, 2, 5);
